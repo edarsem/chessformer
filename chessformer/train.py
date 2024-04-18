986419b01f868d3b46216d5b04731536e6ae42a8
Edouard Albert-Roulhac <edouardalbert-roulhac@mbp-de-edouard.lan>
2024-04-18 18:46:50 -0400

    prints and search (512 best batch size with those params)

--- chessformer/train.py
+++ chessformer/train.py
@@ -2,14 +2,18 @@ import torch
 from torch.utils.data import DataLoader
 from torch.optim import Adam
 from torch.nn import CrossEntropyLoss
+
 from dataset import ChessDataset, collate_fn
 from model import ChessModel
 
-def train(model, data_loader, device, epochs=2, max_steps=1000):
+import time
+
+def train(model, data_loader, device, epochs=2, max_steps=1e4):
     model.train()
     model.to(device)
     optimizer = Adam(model.parameters(), lr=0.001)
     criterion = CrossEntropyLoss()
+    time_start = time.time()
 
     print(f"Training on {device}")
     steps = 0
@@ -31,12 +35,13 @@ def train(model, data_loader, device, epochs=2, max_steps=1000):
             optimizer.step()
             print(f'Epoch {epoch+1}, Step {steps+1}, Loss: {loss.item():.4f}')
             steps += 1
+    print(f'Time per epoch: {(time.time() - time_start) / epochs:.2f} seconds')
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
     
     dataset = ChessDataset('tmp/tokenized_xfen.txt')
-    data_loader = DataLoader(dataset, batch_size=256, shuffle=True, collate_fn=collate_fn)
+    data_loader = DataLoader(dataset, batch_size=512, shuffle=True, collate_fn=collate_fn)
     
     model = ChessModel()
-    train(model, data_loader, device)
+    train(model, data_loader, device, epochs=10)
